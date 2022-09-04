26f82b16d04e76ae0d11f8ed9a8e0870b9d1a7bc
Kirill Koval <knkirill46@gmail.com>
2022-09-04 14:32:26 +0300

    variable name refactor

--- src/main/java/com/github/kerrrusha/order_book/StringUtils.java
+++ src/main/java/com/github/kerrrusha/order_book/StringUtils.java
@@ -1,7 +1,7 @@
 package com.github.kerrrusha.order_book;
 
 public class StringUtils {
-    public static int countMatches(String str, String findStr) {
-        return str.split(findStr, -1).length - 1;
+    public static int countMatches(String str, String substr) {
+        return str.split(substr, -1).length - 1;
     }
 }
